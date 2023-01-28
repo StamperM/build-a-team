47093b19132f7954dec69e7e19a152ac5e731181
Melissa Gillen <melissagillen@Melissas-MacBook-Pro.local>
2023-01-28 10:20:20 -0500

    updated the questions

---
+++ main/test/Employee.test.js
@@ -0,0 +1,28 @@
+const Employee= require("./Employee.js")
+// name can not be empty
+test('Name can not be empty',()=>{
+// setup
+    const employee=new Employee("Ron Swanson", 89, "ron.swanson@gmail.com")
+
+    // logic
+
+    // actual comparision compuicates the the values to Jest so it can validate.
+    expect(employee.name).toBe("Ron Swanson")
+})
+
+test("validate email has correct syntax",()=>{
+    const employee=new Employee("Ron Swanson", 89, "ron.swanson@gmail.com") 
+    //logic
+
+    // actual
+    expect(employee.email).toContain("@")
+})
+
+
+// validate email has correct syntax
+
+
+
+// employee id = number
+
+
