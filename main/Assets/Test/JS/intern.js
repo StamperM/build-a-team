29f17cceeb17543d1c99e10b373e33323ef1ea74
Melissa Gillen <melissagillen@Melissas-MacBook-Pro.local>
2023-01-27 21:35:50 -0500

    added and updated files setting up structure.

--- main/Assets/Test/JS/intern.js
+++ main/Assets/Test/JS/intern.js
@@ -1,7 +1,10 @@
-const employee=require("./Employee")
+const employee=require("./Assets/Test/JS/Employee")
 class intern extends employee{
     constuctor(name,employeeId,email,school){
         super(name,employeeId,email);
             this.school=school;
         }
     }
+
+    module.exports=intern;
+    
---
+++ main/Public/index.js
@@ -0,0 +1,9 @@
+const employee= require("./Assets/Test/JS/Employee");
+const intern = require("./intern");
+const engineer =require("./engineer");
+
+
+
+
+
+
